f3a218978b04a7aad82a5623467a4fca49902af3
Priya Thakur <74084538+priyathakur1234@users.noreply.github.com>
2023-07-11 07:45:15 +0545

    Add files via upload

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,17 @@
+const initState = {
+  projects: [
+    {id: '1', title: 'I am an intern', content: 'blah blah blah'},
+    {id: '2', title: 'working at sandconsole', content: 'blah blah blah'},
+    {id: '3', title: 'started from june', content: 'blah blah blah'}
+  ]
+}
+
+const projectReducer = (state = initState, action) => {
+ 
+ 
+
+  return state;
+}
+
+
+export default projectReducer;
